cc417879388ebb33ab08511e5951056bf3414eba
maddran <madhavnarendran@gmail.com>
2021-03-30 22:42:05 -0600

    update helper_funcs.py

--- helper_funcs.py
+++ helper_funcs.py
@@ -53,68 +53,71 @@ def format_time(elapsed):
     return str(datetime.timedelta(seconds=elapsed_rounded))
     
 
-def predict(data_loader, model):
-  predictions = []
-  true_labels = []
+def predict(data_loader, model, label_flag = True):
+    predictions = []
+    true_labels = []
 
-  for batch in data_loader:
-    input_ids = batch['input_ids'].to(device)
-    attention_mask = batch['attention_mask'].to(device)
-    labels = batch['labels'].to(device)
+    for batch in data_loader:
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
 
-    with torch.no_grad(): 
-      outputs = outputs = model(input_ids, 
-                                attention_mask=attention_mask, 
-                                token_type_ids=None) 
+        with torch.no_grad(): 
+            outputs = model(input_ids, 
+                                    attention_mask=attention_mask, 
+                                    token_type_ids=None) 
 
-    logits = outputs[0]                   
-    logits = logits.detach().cpu().numpy()
-    label_ids = labels.to('cpu').numpy()
+        logits = outputs[0]                   
+        logits = logits.detach().cpu().numpy()
 
-    predictions.append(logits)
-    true_labels.append(label_ids)
-
-  return predictions, true_labels
+        predictions.append(logits)
+        if label_flag:
+            label_ids = labels.to('cpu').numpy()
+            true_labels.append(label_ids)
+    if label_flag:
+        return predictions,true_labels
+    else:
+        return predictions
 
 
 def get_le():
-  import numpy as np
-  le = LabelEncoder()
-  le.classes_ = np.load('classes.npy')
-  return le
+    import numpy as np
+    le = LabelEncoder()
+    le.classes_ = np.load('classes.npy')
+    return le
 
 
 def kth_largest(predictions_i, k, le=get_le()):
-  kth = np.argsort(-predictions_i, axis=1)[:, k-1]
-  preds = le.inverse_transform(list(kth))
-  if k != 1:
-    mask = np.where(np.bincount(np.where(predictions_i > 0)[0]) != k)[0]
-    preds = np.array(
-        [None if i in mask else val for i, val in enumerate(preds)])
+    kth = np.argsort(-predictions_i, axis=1)[:, k-1]
+    preds = le.inverse_transform(list(kth))
+    if k != 1:
+        mask = np.where(np.bincount(np.where(predictions_i > 0)[0]) != k)[0]
+        preds = np.array(
+            [None if i in mask else val for i, val in enumerate(preds)])
 
-  return preds
+    return preds
 
 
 def predict_pipeline(text, labels, model, le=get_le()):
 
-  batch_size = min(len(text), 16)
+    batch_size = min(len(text), 16)
 
-  encoded = encode_text(text, tokenizer)
-  encoded_labels = le.transform(labels)
-  dataset = Dataset(encoded, encoded_labels)
-  loader = DataLoader(dataset, batch_size=batch_size)
-  predictions, true_labels = predict(loader, model)
+    encoded = encode_text(text, tokenizer)
+    encoded_labels = le.transform(labels)
+    dataset = Dataset(encoded, encoded_labels)
+    loader = DataLoader(dataset, batch_size=batch_size)
+    predictions, true_labels = predict(loader, model)
 
-  preds = [np.argmax(predictions[i], axis=1).flatten()
-           for i in range(len(true_labels))]
-  flat_preds = np.concatenate(preds).ravel()
-  pred_cats1 = le.inverse_transform(flat_preds)
+    preds = [np.argmax(predictions[i], axis=1).flatten()
+            for i in range(len(true_labels))]
+    flat_preds = np.concatenate(preds).ravel()
+    pred_cats1 = le.inverse_transform(flat_preds)
 
-  true_labels = np.concatenate(true_labels).ravel()
-  true_cats = le.inverse_transform(true_labels)
+    true_labels = np.concatenate(true_labels).ravel()
+    true_cats = le.inverse_transform(true_labels)
 
-  pred_cats2 = [kth_largest(predictions[i], 2, le).flatten()
-                for i in range(len(predictions))]
-  pred_cats2 = np.concatenate(pred_cats2).ravel()
+    pred_cats2 = [kth_largest(predictions[i], 2, le).flatten()
+                    for i in range(len(predictions))]
+    pred_cats2 = np.concatenate(pred_cats2).ravel()
 
-  return pred_cats1, pred_cats2, true_cats
+    return pred_cats1, pred_cats2, true_cats
